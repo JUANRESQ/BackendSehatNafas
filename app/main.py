b460c3a12e397521cd99fb94c9b850deedea8b44
JUANRESQ <juan.risq@gmail.com>
2025-12-31 21:38:46 +0800

    update main

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
+import os
 from fastapi import FastAPI
+
 from app.db import Base, engine
-from app import models  # <-- PENTING: bikin model kebaca dulu
+from app import models  # supaya SQLAlchemy load model
 
 from app.routes.auth_routes import router as auth_router
 from app.routes.analysis_routes import router as analysis_router
 from app.routes.predict_routes import router as predict_router
-from app.routes.analysis_routes import router as analysis_router
-
-
 
 app = FastAPI(title="SehatNafas API")
 
+# create table (aman untuk project kecil)
 Base.metadata.create_all(bind=engine)
 
+# register routers
 app.include_router(auth_router)
 app.include_router(analysis_router)
 app.include_router(predict_router)
-app.include_router(analysis_router)
-
 
 @app.get("/")
 def root():
